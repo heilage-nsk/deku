ddb9fc473c08dc927e4ebf163e1c83fa4b00ddb4
Oleg Klimenko <o.klimenko@2gis.ru>
2016-05-19 15:27:54 +0600

    Add path-based virtualdom restoration on client-side

--- src/app/index.js
+++ src/app/index.js
@@ -25,40 +25,15 @@ export function createApp (container, handler = noop, options = {}) {
 
   let create = (vnode, context) => {
     if (container){
-      if(container.childNodes.length === 0){
+      if (container.childNodes.length === 0) {
         node = dom.createElement(vnode, rootId, dispatch, context)
         container.appendChild(node)
-      }else{
+      } else {
         let {DOMnode, attachEvents} = dom.createElementThenEvents(vnode, rootId, dispatch, context)
-        let isRenderedCorrectly = true
-
-/*
- * TODO: пропатчить либу, чтобы были минимально возможные изменения при восстановлении из html.
- *
- * Сейчас при несовпадении html (даже если несовпадение _только_ в дочерних нодах) - ререндерится вообще ВСЁ. Это не то, что нам бы хотелось.
- * Идеи:
- * - Создать из текущего html дерево native-vnode и сравнить диффом со сгенерированным из клиентских компонентов.
- *   После чего просто пройтись диффом! Кажется, тут могут всплыть подводные камеи: как понять, куда монтируется компонентная нода?
- * - При создании каждой компонентной ноды проверять наличие аналогичной в дереве и применять дифф.
- *   Разобраться, как создаются нативные ноды из компонентных!
- */
-
-    //    if(container.attributes.checksum){
-    //      isRenderedCorrectly = container.attributes.checksum ===  adler32(DOMnode.outerHTML)
-    //    }else if(container.attributes.autoFix){
-          isRenderedCorrectly = container.innerHTML ===  DOMnode.outerHTML
-          console.log(container.innerHTML, DOMnode.outerHTML);
-    //    }
-
         node = DOMnode
-        if(isRenderedCorrectly){
-          attachEvents(container.firstChild)
-        }else{
-          container.innerHTML = ''
-          container.appendChild(node)
-        }
+        attachEvents(container.firstChild)
       }
-    }else{
+    } else {
       node = dom.createElement(vnode, rootId, dispatch, context)
     }
     oldVnode = vnode
